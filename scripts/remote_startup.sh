234929ccac1d378d870ebfc345da77606b8548b6
Niall Devlin <niallcdevlin@gmail.com>
2023-06-20 16:34:25 -0600

    Nix testing

--- scripts/remote_startup.sh
+++ scripts/remote_startup.sh
@@ -6,8 +6,7 @@ set -x
 # Always run as root.
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
-
-while getopts 'rj' OPTION; do
+while getopts 'r' OPTION; do
     case "$OPTION" in
         r)
             CARL_ADDRESS=$(cat /etc/hosts | grep -w carl | awk '{print $1;}')
@@ -18,46 +17,11 @@ while getopts 'rj' OPTION; do
             # Some of us have more than one wireless card
             MY_ADDRESS=$(ip route get $CARL_ADDRESS | sed 's/.* src //;s/ .*//;2,$d')
 
-            param1=(
-                -e ROS_IP=$MY_ADDRESS
-                -e ROS_MASTER_URI=http://jetson:11311
-            )
-            ;;
-
-        j)
-            for ((c=32; c>=0; c--)); do
-                file="/dev/input/js$c"
-                if [ -e $file ]; then
-                    param2=("--device=$file")
-                fi
-            done
-            if [ ${#param2[@]} -eq 0 ]; then
-                echo "No joystick found"
-                exit 1
-            fi
+            export ROS_IP $MY_ADDRESS
+            export ROS_MASTER_URI http://jetson:11311
             ;;
     esac
 done
 
-
-# Get active IP address
-ip_addr=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
-
-# Figure out which usb is which
-HUSKY_PORT=$(basename "$(readlink "/dev/serial/by-id/$(ls /dev/serial/by-id | grep Prolific)")")
-IMU_PORT=$(basename "$(readlink "/dev/serial/by-id/$(ls /dev/serial/by-id | grep FTDI)")")
-
-export ROS_MASTER_URI http://$ip_addr:11311
-export ROS_IP $ip_addr
-export HUSKY_LOGITECH 1
-export HUSKY_PORT /dev/$HUSKY_PORT
-export IMU_PORT /dev/$IMU_PORT
-export HUSKY_LASER_3D_ENABLED 1
-
 cd ../ros/catkin_ws/
 catkin build
-roscore
-rosrun --wait husky_bringup install
-roslaunch --wait husky_base base.launch 
-roslaunch --wait velodyne_pointcloud VLP16_points.launch
-roslaunch --wait vectornav vectornav.launch
--- shell.nix
+++ shell.nix
@@ -1,11 +1,81 @@
 { pkgs ? import (fetchTarball "https://github.com/lopsided98/nix-ros-overlay/archive/master.tar.gz") {} }:
+with pkgs;
+with rosPackages.noetic;
+with pythonPackages;
 
-pkgs.mkShell {
+mkShell {
   buildInputs = [
-    pkgs.curl
-    pkgs.ros-noetic-desktop-full
-    pkgs.which
-    pkgs.htop
-    pkgs.zlib
+    clang
+    gnumake
+    gcc
+    git
+    tmux
+    catkin-tools
+    rosdep
+    eigen
+    glibcLocales
+    pcl
+    libyaml
+    scipy
+    (buildEnv { paths = [
+      rosbash
+      roscpp
+      xacro
+	    rosbag
+      plotjuggler
+      rqt-tf-tree
+      rqt-plot
+      rqt-image-view 
+      dynamic-reconfigure
+      rqt-reconfigure
+      ros-control
+      diagnostics
+      roslint
+      robot-upstart
+      robot-state-publisher
+      velodyne-description
+      robot-localization
+      interactive-marker-twist-server
+      twist-mux
+      teleop-twist-joy
+      joy
+      joint-state-controller
+      diff-drive-controller
+      joy-teleop
+      velodyne
+      realsense2-description
+      joint-state-publisher
+      joint-state-publisher-gui
+      rviz
+      rviz-imu-plugin
+      rqt-robot-monitor
+      pcl-ros
+      joint-trajectory-controller
+      teleop-twist-keyboard
+      imu-filter-madgwick
+      imu-transformer
+      microstrain-3dmgx2-imu
+      microstrain-inertial-driver
+      nmea-comms
+      nmea-navsat-driver
+      realsense2-camera
+      spinnaker-camera-driver
+      um6
+      um7
+      urg-node
+      gazebo-plugins
+      gazebo-ros
+      gazebo-ros-control
+      hector-gazebo-plugins
+      pointcloud-to-laserscan
+      velodyne-gazebo-plugins
+      amcl
+      gmapping
+      map-server
+      move-base
+      navfn
+      base-local-planner
+      dwa-local-planner
+    ]; })
   ];
 }
