5b871fb12d96be835179f0dca9b4278c6499514e
Niall Devlin <niallcdevlin@gmail.com>
2023-06-21 10:18:29 -0600

    Nix working

--- shell.nix
+++ shell.nix
@@ -55,4 +55,23 @@ mkShell {
       base-local-planner
     ]; })
   ];
+  shellHook = ''
+    # Get active IP address
+    ip_addr=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+    export ROS_IP=$ip_addr
+    export HUSKY_LOGITECH=1
+    export HUSKY_LASER_3D_ENABLED=1
+    if [ "$(hostname)" = "husky" ]; then
+      export ROS_MASTER_URI=http://$ip_addr:11311
+      # Figure out which usb is which
+      HUSKY_PORT=$(basename "$(readlink "/dev/serial/by-id/$(ls /dev/serial/by-id | grep Prolific)")")
+      IMU_PORT=$(basename "$(readlink "/dev/serial/by-id/$(ls /dev/serial/by-id | grep FTDI)")")
+      export HUSKY_PORT=/dev/$HUSKY_PORT
+      export IMU_PORT=/dev/$IMU_PORT
+    else
+      export ROS_MASTER_URI=http://jetson:11311
+    fi
+    cd ros/catkin_ws
+    catkin_make
+  '';
 }
