80ce778c821f13088058ed10feaa58c24276c682
Niall Devlin <niallcdevlin@gmail.com>
2023-06-21 12:11:03 -0600

    Making nix smaller does not build on jetson

--- shell.nix
+++ shell.nix
@@ -23,34 +23,17 @@ mkShell {
       roscpp
       xacro
 	    rosbag
-      rqt-image-view 
       rqt-reconfigure
-      ros-control
-      diagnostics
-      roslint
-      robot-upstart
       robot-state-publisher
       velodyne-description
-      robot-localization
-      interactive-marker-twist-server
-      twist-mux
       teleop-twist-joy
       joy
       joint-state-controller
       diff-drive-controller
       velodyne
-      realsense2-description
       joint-state-publisher
-      joint-state-publisher-gui
       pcl-ros
-      joint-trajectory-controller
       teleop-twist-keyboard
-      imu-filter-madgwick
-      imu-transformer
-      nmea-comms
-      nmea-navsat-driver
-      realsense2-camera
-      pointcloud-to-laserscan
       move-base
       base-local-planner
     ]; })
