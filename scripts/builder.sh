a94286c107fff156631fb3f65477b1edb4677020
Niall Devlin <niallcdevlin@gmail.com>
2023-06-26 17:07:07 -0600

    Updated urdf

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Always run in catkin_ws
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path/../ros/catkin_ws"
+# parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+# cd "$parent_path/../ros/catkin_ws"
 
 # Get active IP address
 ip_addr=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
@@ -10,6 +10,8 @@ export ROS_IP=$ip_addr
 
 export HUSKY_LOGITECH=1
 export HUSKY_LASER_3D_ENABLED=1
+export HUSKY_LASER_3D_PREFIX=ouster
+
 
 if [ "$(hostname)" = "husky" ]; then
   export ROS_MASTER_URI=http://$ip_addr:11311
@@ -22,15 +24,6 @@ else
   export ROS_MASTER_URI=http://husky:11311
   alias start_joy="roslaunch husky_teleop teleop.launch"
 fi
-
-SETUP="devel/setup.bash"
-if [ -e "$SETUP" ]; then
-  source $SETUP
-else
-  echo "Please catkin_make and source devel/setup.bash"
-fi
-
-source /opt/ros/noetic/setup.bash
 
 cmake_args=(
   -DCMAKE_BUILD_TYPE=Release
