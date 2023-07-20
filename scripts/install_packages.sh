579e52292ec19d9715143425837d2879dc306c26
Niall Devlin <niallcdevlin@gmail.com>
2023-07-20 14:31:04 -0600

    updated install script

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -4,9 +4,14 @@ set -eou pipefail
 export DEBIAN_FRONTEND='noninteractive'
 
 add-apt-repository -y ppa:borglab/gtsam-release-4.0
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-get install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 apt-get update -y
 apt-get upgrade -y
+
+apt-get install -y ros-noetic-desktop-base
 
 apt-get install -y ros-noetic-urdf
 apt-get install -y ros-noetic-xacro
