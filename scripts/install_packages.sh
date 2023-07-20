c738bb9cae94b88e75f5753402e82f530cde6f33
Niall Devlin <niallcdevlin@gmail.com>
2023-07-20 14:58:02 -0600

    updated install script

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 export DEBIAN_FRONTEND='noninteractive'
 
 sudo add-apt-repository -y ppa:borglab/gtsam-release-4.0
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-get install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
