6f53507c895765cce23e51e06ddad723458067bf
sgoodyear <96261962+sgoodyear@users.noreply.github.com>
2024-02-02 13:48:45 -0700

    Update install_packages_host.sh

--- scripts/install_packages_host.sh
+++ scripts/install_packages_host.sh
@@ -3,12 +3,6 @@
 set -eou pipefail
 export DEBIAN_FRONTEND='noninteractive'
 
-_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-get install curl -y
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
