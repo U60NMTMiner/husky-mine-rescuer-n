454966b9941ecb04d0fee43b8ed39856220bd26b
Niall Devlin <niallcdevlin@gmail.com>
2024-02-02 12:40:34 -0700

    scripts

--- scripts/env_ctrl.sh
+++ scripts/env_ctrl.sh
@@ -1,6 +1,5 @@
 _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${_dir}/builder.sh
-source ${_dir}/../ros/catkin_ws/devel/setup.zsh
 
 export ROS_MASTER_URI="http://$(getip husky):11311"
 export ROS_MASTER_IP="$(getip husky)"
